c9a5f0113c4de7c8547a008b5b8e4cca634a3e4f
AdnanMahida <52555425+AdnanMahida@users.noreply.github.com>
2023-07-05 00:08:31 +0530

    integrated account,privacy screen

---
+++ lib/modules/setting/screen/privacy_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_whatsapp/modules/setting/setting_controller.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
+
+import '../../../themes/colors_theme.dart';
+
+PrivacyScreen(SettingController controller) {
+  return GetBuilder<SettingController>(
+      builder: (controller) => ListView.builder(
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return const ListTile(
+                leading: Stack(
+                  children: <Widget>[
+                    Icon(
+                      Icons.link,
+                      size: 20.0,
+                      color: ThemeColor.white,
+                    ),
+                  ],
+                ),
+                title: Text('Create call link'),
+                subtitle: Text('Share a link for your WhatsApp calls'),
+              );
+              /* return Column(
+            children: [
+              ListTile(
+                leading: CircleAvatar(
+                  foregroundColor: Theme.of(context).primaryColor,
+                  backgroundColor: Colors.grey,
+                  backgroundImage:
+                  NetworkImage(controller.chatList[index].iconUrl),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      controller.chatList[index].title,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      controller.chatList[index].lastMessage,
+                      style: const TextStyle(
+                          color: Colors.grey, fontSize: 14.0),
+                    ),
+                  ],
+                ),
+                subtitle: Container(
+                  padding: const EdgeInsets.only(top: 5.0),
+                  child: Text(
+                    controller.chatList[index].time,
+                    style:
+                    const TextStyle(color: Colors.grey, fontSize: 15.0),
+                  ),
+                ),
+              ),
+              const Divider(
+                height: 10.0,
+              ),
+            ],
+          );*/
+            },
+          ));
+}
